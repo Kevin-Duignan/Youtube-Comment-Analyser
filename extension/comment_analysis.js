6674fe529cf12c3ccd01b37bc905b388d04fc786
Anthony Wilson <awilsonprogramming@gmail.com>
2023-09-09 11:28:07 +1000

    Added basic browser extension.

---
+++ extension/comment_analysis.js
@@ -0,0 +1,12 @@
+
+"use strict";
+
+console.log("Extension is active");
+
+var url = new URL(window.location.href);
+console.log("Video ID:", url.searchParams.get("v"));
+
+
+//var comments_container = document.getElementById("comments");
+//console.log(comments_container);
+
