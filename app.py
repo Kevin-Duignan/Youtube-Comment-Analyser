b0d11fc25c3f429662e5eb2aad83a09ca98d0cce
Kevin Duignan <kevin.nguyen200476@gmail.com>
2023-09-09 12:33:25 +1000

    Add new route handler for get req with video id

--- app.py
+++ app.py
@@ -1,28 +1,56 @@
 from flask import Flask, jsonify
 from googleapiclient.discovery import build
+import googleapiclient
+from analysis import AnalysisSingleton
 import json
 
 app = Flask(__name__)
-api_key = "API_KEY"
-youtube_service = build("youtube", "v3", developerKey=api_key)
+api_key = "AIzaSyBtxJEet5gIXlbXD2gtCKwcss98JnT-Am0"
+youtube = build("youtube", "v3", developerKey=api_key)
 
 
 @app.route("/<video_id>")
-def get_comments(video_id):
-    request = youtube_service.commentThreads().list(
-        part="snippet,replies",
-        videoId=video_id,
-        maxResults=100,  # Google's API has a limit on the max results
-    )
+def get_comments(video_id: str):
+    request = youtube.commentThreads().list(part="snippet, replies", videoId=video_id, maxResults=100)
     response = request.execute()
 
+    all_comments = []
+    all_top_comments = []
+
+    comments, top_comments = process_comments(response)
+    all_comments += comments
+    all_top_comments += top_comments
+
+    # if nextPageToken, call API again, until max size is reached
+    i = 0
+    while i < 5 and response.get("nextPageToken", None):
+        request = youtube.commentThreads().list(part="snippet", videoId=video_id, pageToken=response["nextPageToken"])
+        response = request.execute()
+        comments, top_comments = process_comments(response)
+        all_comments += comments
+        all_top_comments += top_comments
+        i += 1
+
+    # Writes comments to JSON file
+    with open("comments.json", "w", encoding="utf-8") as f:
+        json.dump(all_comments, f, ensure_ascii=False, indent=4)
+
+    for i, val in enumerate(all_top_comments):
+        print(str(i) + "--->" + val)
+
+    # sentiment_analysis(top_comments)
+    return jsonify(all_comments)
+
+
+def process_comments(response):
     comments = []
+    top_comments = []
     for item in response["items"]:
         comment_data = item["snippet"]["topLevelComment"]["snippet"]
         comment = {
             "author": comment_data["authorDisplayName"],
             "publishedAt": comment_data["publishedAt"],
-            "text": comment_data["textDisplay"],
+            "text": comment_data["textOriginal"],
             "likes": comment_data["likeCount"],
             "replies": [],
         }
@@ -32,16 +60,20 @@ def get_comments(video_id):
                 reply = {
                     "author": reply_data["authorDisplayName"],
                     "publishedAt": reply_data["publishedAt"],
-                    "text": reply_data["textDisplay"],
+                    "text": reply_data["textOriginal"],
                     "likes": reply_data["likeCount"],
                 }
                 comment["replies"].append(reply)
         comments.append(comment)
-    # Writes comments to JSON file
-    with open("comments.json", "w", encoding="utf-8") as f:
-        json.dump(comments, f, ensure_ascii=False, indent=4)
+        top_comments.append(comment["text"])
+    return comments, top_comments
 
-    return jsonify(comments)
+
+def sentiment_analysis(comments: list[str]):
+    analyser = AnalysisSingleton()
+    print(analyser.calculate_sentiment_statistics(comments))
+    print(analyser.calculate_emotion_statistics(comments))
+    print(analyser.calculate_derision_statistics(comments))
 
 
 if __name__ == "__main__":
