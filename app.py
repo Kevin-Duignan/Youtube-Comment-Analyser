d2acc1039395b5590db2d7687c7db1f0024f32e0
Kevin Duignan <kevin.nguyen200476@gmail.com>
2023-09-09 13:15:47 +1000

    Separate processing comments code

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from googleapiclient.discovery import build
 import googleapiclient
 from analysis import AnalysisSingleton
+from comments import CommentProcessor
 import json
 
 app = Flask(__name__)
@@ -11,69 +12,9 @@ youtube = build("youtube", "v3", developerKey=api_key)
 
 @app.route("/<video_id>")
 def get_comments(video_id: str):
-    request = youtube.commentThreads().list(part="snippet, replies", videoId=video_id, maxResults=100)
-    response = request.execute()
-
-    all_comments = []
-    all_top_comments = []
-
-    comments, top_comments = process_comments(response)
-    all_comments += comments
-    all_top_comments += top_comments
-
-    # if nextPageToken, call API again, until max size is reached
-    i = 0
-    while i < 5 and response.get("nextPageToken", None):
-        request = youtube.commentThreads().list(part="snippet", videoId=video_id, pageToken=response["nextPageToken"])
-        response = request.execute()
-        comments, top_comments = process_comments(response)
-        all_comments += comments
-        all_top_comments += top_comments
-        i += 1
-
-    # Writes comments to JSON file
-    with open("comments.json", "w", encoding="utf-8") as f:
-        json.dump(all_comments, f, ensure_ascii=False, indent=4)
-
-    for i, val in enumerate(all_top_comments):
-        print(str(i) + "--->" + val)
-
-    # sentiment_analysis(top_comments)
-    return jsonify(all_comments)
-
-
-def process_comments(response):
-    comments = []
-    top_comments = []
-    for item in response["items"]:
-        comment_data = item["snippet"]["topLevelComment"]["snippet"]
-        comment = {
-            "author": comment_data["authorDisplayName"],
-            "publishedAt": comment_data["publishedAt"],
-            "text": comment_data["textOriginal"],
-            "likes": comment_data["likeCount"],
-            "replies": [],
-        }
-        if item.get("replies"):
-            for reply in item["replies"]["comments"]:
-                reply_data = reply["snippet"]
-                reply = {
-                    "author": reply_data["authorDisplayName"],
-                    "publishedAt": reply_data["publishedAt"],
-                    "text": reply_data["textOriginal"],
-                    "likes": reply_data["likeCount"],
-                }
-                comment["replies"].append(reply)
-        comments.append(comment)
-        top_comments.append(comment["text"])
-    return comments, top_comments
-
-
-def sentiment_analysis(comments: list[str]):
-    analyser = AnalysisSingleton()
-    print(analyser.calculate_sentiment_statistics(comments))
-    print(analyser.calculate_emotion_statistics(comments))
-    print(analyser.calculate_derision_statistics(comments))
+    cp = CommentProcessor(api_key)
+    top_comments = cp.get_comment_threads(video_id)
+    return jsonify(top_comments)
 
 
 if __name__ == "__main__":
--- comments.py
+++ comments.py
@@ -1,48 +1,47 @@
 from googleapiclient.discovery import build
 import json
 
-class CommentProcessor:  
+
+class CommentProcessor:
     def __init__(self, api_key: str):
         self.API_KEY = api_key
         self.youtube_service = build("youtube", "v3", developerKey=api_key)
-    
+
     def get_comment_threads(self, videoID: str, pages: int = 1, rich_comment: bool = False) -> list[str]:
         comments_list = []
-        
+
         request = self.youtube_service.commentThreads().list(
             part="snippet,replies",
             videoId=videoID,
             maxResults=100,  # Google's API has a limit on the max results
         )
         response = request.execute()
-        
+
         comments_list += self._process_comments(response, rich_comment)
 
         # if nextPageToken, call API again, until pages is reached
         i = 0
-        while i < pages and response.get('nextPageToken', None):
+        while i < pages and response.get("nextPageToken", None):
             request = self.youtube_service.commentThreads().list(
-                part='snippet',
-                videoId=videoID,
-                pageToken=response['nextPageToken']
+                part="snippet", videoId=videoID, pageToken=response["nextPageToken"]
             )
             response = request.execute()
             comments_list += self._process_comments(response, rich_comment)
             i += 1
 
         print(f"Finished fetching comments for {videoID}. {len(comments_list)} comment threads found.")
-        
+
         return comments_list
 
     def _process_comments(self, response: dict, rich_comment: bool = False) -> list[str]:
         res = []
         for item in response["items"]:
             comment_data = item["snippet"]["topLevelComment"]["snippet"]
-            
+
             if not rich_comment:
                 res.append(comment_data["textOriginal"])
                 continue
-            
+
             comment = {
                 "author": comment_data["authorDisplayName"],
                 "publishedAt": comment_data["publishedAt"],
@@ -61,13 +60,12 @@ class CommentProcessor:
                     }
                     comment["replies"].append(reply)
             res.append(comment)
-            
+
         return res
-    
+
+
 if __name__ == "__main__":
     with open("config.json", "r") as jsonfile:
         data = json.load(jsonfile)
-        
     cp = CommentProcessor(data["api_key"])
     print(cp.get_comment_threads("lSD_L-xic9o"))
-    
